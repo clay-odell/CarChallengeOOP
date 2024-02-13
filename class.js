8b1aa9b45fb3dca18d97e5e876e03e642237a450
Clay O'Dell <clay.odell.music@gmail.com>
2024-02-13 16:47:30 -0600

    Part 3 added

--- class.js
+++ class.js
@@ -16,12 +16,30 @@ let myFirstVehicle = new Vehicle("Honda", "Monster Truck", 1999);
 myFirstVehicle.honk(); // "Beep."
 
 class Car extends Vehicle {
-    constructor(make, model, year, numWheels) {
-        super(make, model, year);
-        this.numWheels = 4;
-    }
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 4;
+  }
 }
 let myFirstCar = new Car("Toyota", "Corolla", 2005);
 myFirstCar.toString(); // "The vehicle is a Toyota Corolla from 2005."
-myFirstCar.honk();     // "Beep."
-console.log(myFirstCar.numWheels);  // 4
+myFirstCar.honk(); // "Beep."
+myFirstCar.numWheels; // 4
+
+class Motorcycle extends Vehicle {
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 2;
+  }
+  revEngine() {
+    return "VROOM!!!";
+  }
+}
+let myFirstMotorcycle = new Motorcycle("Honda", "Nighthawk", 2000);
+
+myFirstMotorcycle.toString();
+// "The vehicle is a Honda Nighthawk from 2000."
+
+myFirstMotorcycle.honk();     // "Beep."
+myFirstMotorcycle.revEngine(); // "VROOM!!!"
+myFirstMotorcycle.numWheels;  // 2
