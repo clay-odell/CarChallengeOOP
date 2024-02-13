08d0fbf90e958027808282fe7d52b61edc9bd3e2
Clay O'Dell <clay.odell.music@gmail.com>
2024-02-13 16:41:07 -0600

    Part Two added

--- class.js
+++ class.js
@@ -8,9 +8,20 @@ class Vehicle {
     return "Beep";
   }
   toString() {
-    const {make, model, year } = this;
+    const { make, model, year } = this;
     return `The vehicle is a ${make} ${model} from ${year}.`;
   }
 }
 let myFirstVehicle = new Vehicle("Honda", "Monster Truck", 1999);
 myFirstVehicle.honk(); // "Beep."
+
+class Car extends Vehicle {
+    constructor(make, model, year, numWheels) {
+        super(make, model, year);
+        this.numWheels = 4;
+    }
+}
+let myFirstCar = new Car("Toyota", "Corolla", 2005);
+myFirstCar.toString(); // "The vehicle is a Toyota Corolla from 2005."
+myFirstCar.honk();     // "Beep."
+console.log(myFirstCar.numWheels);  // 4
